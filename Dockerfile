6ba963405e3fb62e87b7a5bde452c0392bb0b103
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2022-09-14 07:54:52 +0430

    Added image field and updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,16 @@ LABEL maintainer="Mohammad-MH1"
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
+RUN apk add jpeg-dev musl-dev zlib zlib-dev
 RUN pip install -r /requirements.txt
 
 RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
 
+RUN mkdir -p /vol/web/media
+RUN mkdir -p /vol/web/static
 RUN adduser -D user
+RUN chown -R user:user /vol/
+RUN chmod -R 755 /vol/web
 USER user
--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
 
+import uuid
+import os
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
+
 
 class UserManager(BaseUserManager):
 
@@ -67,6 +78,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField(Tag)
     ingredients = models.ManyToManyField(Ingredient)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self) -> str:
         return self.title
